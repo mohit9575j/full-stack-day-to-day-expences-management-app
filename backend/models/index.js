2ab14cc7fde8c52ed2e8175b8ca69dc1d8da3e97
Mohit <joshimohit8130@gmail.com>
2025-04-30 04:09:17 +0530

    create Expense object with sequelize instance and models and initialize it with db object

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,11 +2,14 @@
 import { Sequelize } from 'sequelize';
 import sequelize from '../config/db.js';
 import User from './user.model.js';
+import Expense from './expense.model.js';
+
 
 const db = {
   sequelize,
   Sequelize,
-  User
+  User,
+  Expense
 };
 
 // For Sequelize operators like Op.gt
