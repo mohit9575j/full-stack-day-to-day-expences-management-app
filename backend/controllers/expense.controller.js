2782e6e355db36f259aecb717df88286ba960703
Mohit <joshimohit8130@gmail.com>
2025-04-30 03:44:05 +0530

    feat(controller): implement addExpense function with try-catch and response

--- backend/controllers/expense.controller.js
+++ backend/controllers/expense.controller.js
@@ -1,4 +1,22 @@
 import db from '../models/index.js';
 
 const Expense  = db.Expense;
+
+// Add expense
+export const addExpense = async (req, res) => {
+  const { amount, description, category } = req.body;
+
+  try {
+    const newExpense = await Expense.create({
+      amount,
+      description,
+      category,
+    });
+
+    res.status(201).json({ message: 'Expense added successfully', expense: newExpense });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
  
