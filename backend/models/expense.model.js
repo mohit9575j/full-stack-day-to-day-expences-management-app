39982c9a59cbc59d29e044b997013b31837a8fde
mohit <joshimohit8120@gmail.com>
2025-04-30 02:45:10 +0530

    feat(models): add amount field in Expense model

--- backend/models/expense.model.js
+++ backend/models/expense.model.js
@@ -1,6 +1,12 @@
  import { DataTypes } from 'sequelize';
 import sequelize from '../config/db.js';
 
-const Expense = sequelize.define('Expense', {});
-
+ const Expense = sequelize.define('Expense', {
+    amount: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  });
+  
 export default Expense;
+
