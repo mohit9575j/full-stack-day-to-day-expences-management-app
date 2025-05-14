173efcc844bc5fe4c499d13e4b077db7eb09a3a0
Mohit <joshimohit8130@gmail.com>
2025-05-14 23:31:33 +0530

    Update ResetPassword.js

--- frontend/src/components/ResetPassword.js
+++ frontend/src/components/ResetPassword.js
@@ -1,100 +1,4 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import { useNavigate, Link } from "react-router-dom";
-
-
-
-
-//   const  ExpenseForm  = () => {
-//   const [form, setForm] = useState({
-//     name: "",
-//     password: "",
-//   });
-
-//   const [message, setMessage] = useState("");
-//   const navigate = useNavigate();
-
-//   const handleChange = (e) => {
-//     setForm({ ...form, [e.target.name]: e.target.value });
-//   };
-
-//   const handleSubmit = async (e) => {
-//     e.preventDefault();
-    
-
-//     try {
-//       const res = await axios.post("http://localhost:4000/api/auth/reset-password", form);
-//       setMessage(res.data.message);
-//       setForm({ token: "",  password: "" });
-//     navigate("/login");
-//     } catch (err) {
-//       setMessage(err.response?.data?.message || "Something went wrong");
-//     }
-//   };
-
-//   return (
-//     <div className="min-h-screen bg-gray-100 flex items-center justify-center px-4">
-//       <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
-//         <h2 className="text-2xl font-bold mb-6 text-center text-blue-600">Reset Password</h2>
-
-//         <form onSubmit={handleSubmit} className="space-y-4">
-//           <input
-//             type="text"
-//             name="token"
-//             value={form.token}
-//             onChange={handleChange}
-//             placeholder="token"
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-//             required
-//           />
-
-//           <input
-//             type="password"
-//             name="password"
-//             value={form.password}
-//             onChange={handleChange}
-//             placeholder="Password"
-//             className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
-//             required
-//           />
-
-//           <button
-//             type="submit"
-//             className="w-full bg-blue-500 text-white py-2 rounded-lg hover:bg-blue-600 transition"
-//           >
-//             Set New Password
-//           </button>
-//         </form>
-
-//         {message && (
-//           <p className="mt-4 text-center text-sm text-red-500">{message}</p>
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-//  export default  ExpenseForm ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
 
 
 import React, { useState } from "react";
@@ -111,7 +15,7 @@ const ResetPassword = () => {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:4000/api/auth/reset-password", {
+      const res = await axios.post("http://13.203.206.230:4000/api/auth/reset-password", {
         token,
         newPassword
       });
