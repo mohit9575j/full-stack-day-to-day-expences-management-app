6408e96840bae9a708589da3e3f8a1cb4190c133
Mohit <joshimohit8130@gmail.com>
2025-05-17 19:03:56 +0530

    Define and export the Expense model

--- backend/models/expense.model.js
+++ backend/models/expense.model.js
@@ -3,19 +3,19 @@ import sequelize from '../config/db.js';
 
 
  const Expense = sequelize.define('Expense', {
-    amount: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      category: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-  });
-  
+  amount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
 export default Expense;
 
