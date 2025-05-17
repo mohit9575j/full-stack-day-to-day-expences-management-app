88600a80cb88dfd4ec06977b3e755e96994f0477
Mohit <joshimohit8130@gmail.com>
2025-05-17 19:28:17 +0530

    Import Sequelize instance and route modules

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
  import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import sequelize from './config/db.js';
+import authRoutes from './routes/auth.routes.js';
+import expenseRoutes from './routes/expense.routes.js';
+import premiumRoutes from './routes/premium.routes.js';
+import leadRoutes from './routes/lead.routes.js';
+
 
 dotenv.config();
 
