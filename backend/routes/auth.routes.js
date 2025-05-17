decc92dcae2b6eded8983aad34014f8f4cec4c34
Mohit <joshimohit8130@gmail.com>
2025-05-17 19:13:09 +0530

    Import required modules and initialize router

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,15 +1,4 @@
-import express from 'express';
+ import express from 'express';
 import { register, login, resetPasswordRequest, setNewPassword } from '../controllers/auth.controller.js';
 
 const router = express.Router();
-
-
-router.post('/register', register);
-
-router.post('/login', login);
-
-router.post('/reset-password-request', resetPasswordRequest);
-
-router.post('/reset-password', setNewPassword);
-
-export default router;
