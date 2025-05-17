9f060c1163f32b56e0245cdaa9e4688a9a03baa1
Mohit <joshimohit8130@gmail.com>
2025-05-17 19:10:15 +0530

    Setup imports and model declaration with basic fields

--- backend/models/order.model.js
+++ backend/models/order.model.js
@@ -1,1 +1,14 @@
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db.js';
+
+const Order = sequelize.define('Order', {
+  orderAmount: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  orderCurrency: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: 'INR',
+  },
 
