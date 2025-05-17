977be7b4f2ed621db6199790b7a7ee774703f368
Mohit <joshimohit8130@gmail.com>
2025-05-17 19:21:07 +0530

    Module imports and router initialization

--- backend/routes/lead.routes.js
+++ backend/routes/lead.routes.js
@@ -1,1 +1,12 @@
+// External Modules
+import express from 'express';
+
+// Controllers
+import { getLeaderboard } from '../controllers/lead.Controller.js';
+
+// Middleware
+import authMiddleware from '../middleware/authMiddleware.js';
+
+// Initialize Express Router
+const router = express.Router();
 
