40d8a02f1a582875e1c6d6d2bc50a042bea86789
Mohit <joshimohit8130@gmail.com>
2025-05-17 18:16:14 +0530

    feat: Implement user registration with hashed password

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -7,22 +7,22 @@ const User = db.User;
  
 
 
+//  User Register Function
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
-  
-    try {
-      const existing = await User.findOne({ where: { email } });
-      if (existing) return res.status(400).json({ message: "User already exists" });
-  
-      const hashed = await bcrypt.hash(password, 10);
-      await User.create({ name, email, password: hashed });
-  
-      res.status(201).json({ message: "User registered successfully" });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  };
 
+    try {
+        const existing = await User.findOne({ where: { email } });
+        if (existing) return res.status(400).json({ message: "User already exists" });
+
+        const hashed = await bcrypt.hash(password, 10);
+        await User.create({ name, email, password: hashed });
+
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
 
 
 
