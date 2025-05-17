a2aaea70ed4dba0a9e348fe0a10bce19bf60fdc7
Mohit <joshimohit8130@gmail.com>
2025-05-17 20:03:36 +0530

    Middleware function and user fetching logic

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,3 +2,11 @@ import db from '../models/index.js';
 
 const User = db.User;
 
+const premiumMiddleware = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
