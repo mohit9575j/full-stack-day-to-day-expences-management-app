ea2a0fbc02a2acb23d4f289a43bfbd65bf225a05
Mohit <joshimohit8130@gmail.com>
2025-05-18 13:54:59 +0530

    Sigh in page for user login

--- frontend/src/pages/SignInPage.js
+++ frontend/src/pages/SignInPage.js
@@ -1,1 +1,8 @@
+import React from 'react';
+import Register from '../components/Register';  // Capital "R" se import karo
 
+ function SignInPage() {
+  return <Register />;  // JSX me component ko tag ke form me return karo
+}
+
+export default SignInPage;
