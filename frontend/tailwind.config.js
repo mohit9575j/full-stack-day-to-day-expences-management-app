1eab828e4af85f2b2c2bdfb5da2016da2f17a37a
Mohit <joshimohit8130@gmail.com>
2025-05-18 13:30:14 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Yeh paths specify kar raha hai jahan Tailwind ko styles apply karne hain
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
    
