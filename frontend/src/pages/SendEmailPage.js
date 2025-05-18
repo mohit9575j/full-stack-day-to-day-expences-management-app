c3b5ee937e46d83b46ac6ca8f94be402be643621
Mohit <joshimohit8130@gmail.com>
2025-05-18 13:54:18 +0530

    In this page user can send email of for reset their password

--- frontend/src/pages/SendEmailPage.js
+++ frontend/src/pages/SendEmailPage.js
@@ -1,1 +1,9 @@
 
+import React from 'react';
+import SendEmail from '../components/SendEmail';  // Capital "R" se import karo
+
+ function SendEmailPage() {
+  return <SendEmail />;  // JSX me component ko tag ke form me return karo
+}
+
+export default SendEmailPage;
